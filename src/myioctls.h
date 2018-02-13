4b2b4fce16329a96d2c7b7a2a87b8b6790d49a82
Kivanc Cakmak <kivanccakmak@gmail.com>
2018-02-14 00:31:38 +0300

    remove old files

--- src/myioctls.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MY_IOCTLS_H
-#define _MY_IOCTLS_H
-
-int get_iface_ip(const char *ifname, char *ip_out);
-int get_iface_mac(const char *ifname, char *mac_out);
-int get_iface_flags(const char *ifname, char *flags_out, int *num_flags);
-
-#endif
